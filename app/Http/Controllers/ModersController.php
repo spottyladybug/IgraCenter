3e9f7d2fc75873d936d41a4ed36488a775473079
anna <lyalinanna@gmail.com>
2018-06-12 12:59:07 +0300

    Авторизация через VK. Созданы 2 новых контроллера для авторизации Moders и Players.

--- app/Http/Controllers/ModersController.php
+++ app/Http/Controllers/ModersController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Moders;
 
 class ModersController extends Controller
 {
-    //
+    public function findModer($user)
+    {
+        
+        $authModer = Moders::where('vk_id_mod', $user->id)->first();
+        if ($authModer) {
+            return $authModer;
+        }
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-// OAuth Routes
-Route::get('login/vkontakte', 'Auth\LoginController@redirectToProvider');
-Route::get('login/vkontakte/callback', 'Auth\LoginController@handleProviderCallback');
+// OAuth Routes for Moders
+Route::get('login/vkontakte/moder', 'Auth\LoginModersController@redirectToProvider');
+Route::get('login/vkontakte/callback/moder', 'Auth\LoginModersController@handleProviderCallback');
+
+// OAuth Routes for Players
+Route::get('login/vkontakte/players', 'Auth\LoginPlayersController@redirectToProvider');
+Route::get('login/vkontakte/callback/players', 'Auth\LoginPlayersController@handleProviderCallback');
