878896ef97ccd83b7a40b20315a237c70ede71d9
anna <lyalinanna@gmail.com>
2018-06-16 23:26:33 +0300

    Удаление зачем-то созданной модели, идентичной существующей

--- resources/views/timer.blade.php
+++ resources/views/timer.blade.php
@@ -12,7 +12,7 @@
 <form method="POST" action="/setTimer">
     {!! csrf_field() !!}
     <input type="submit" name="startbyn" value="ПРИШЛА КОМАНДА!">
-    <input name='id_st_timer' type='text' hidden value="{{\App\Users_moder::where('id_user_moder',Auth::id())->value('id_station_moder')}}">
+    <input name='id_st_timer' type='text' hidden value="{{\App\Moders::where('id_user_moder',Auth::id())->value('id_station_moder')}}">
     <input name='id_moder_timer' type='text' hidden value="{{Auth::id()}}">
 </form>
 Секундомер:
