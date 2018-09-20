dcaa919a5ce70267d5ce3ac14b4e573b78d5dd17
anna <lyalinanna@gmail.com>
2018-09-20 22:58:10 +0300

    Аватарка

--- database/migrations/2018_06_14_053713_create_users_table.php
+++ database/migrations/2018_06_14_053713_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name_user');
             $table->string('vk_id_user');
             $table->integer('group_user');
+            $table->text('avatar');
         });
     }
 
